02f3e876d7dd46efc78be89e29f27121e67de841
James-W-Berry <james.berry.iv@gmail.com>
2020-08-14 17:53:34 -0400

    add spotify token check to onboarding

--- app/src/main/java/com/berryspace/conjure/OnboardingFragment.java
+++ app/src/main/java/com/berryspace/conjure/OnboardingFragment.java
@@ -7,50 +7,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.material.button.MaterialButton;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 
 public class OnboardingFragment extends Fragment {
-    int pageNumber = 0;
+    private final static String TAG = "OnboardingFragment";
+    private int pageNumber = 0;
+    private Boolean validToken = false;
+
     public OnboardingFragment(int page){
         this.pageNumber = page;
     }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-        ViewGroup view;
-        switch (pageNumber){
-            case 0:
-                 view = (ViewGroup) inflater.inflate(
-                        R.layout.fragment_onboarding_welcome, container, false);
-                break;
-            case 1:
-                 view = (ViewGroup) inflater.inflate(
-                        R.layout.fragment_onboarding_setup, container, false);
-                break;
-            case 2:
-                 view = (ViewGroup) inflater.inflate(
-                        R.layout.fragment_onboarding_scan, container, false);
-                MaterialButton finish = view.findViewById(R.id.button_finish);
-                finish.setOnClickListener(v -> {
-                    System.out.println("recording user finished onboarding");
-                    SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getSharedPreferences("NEWUSER",Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putBoolean("newUser", false);
-                    editor.apply();
-                    System.out.println("starting HomeActivity");
-                    Intent intent = new Intent(getActivity(), HomeActivity.class);
-                    startActivity(intent);
-                });
-                break;
-            default:
-                view = (ViewGroup) inflater.inflate(
-                        R.layout.fragment_onboarding_welcome, container, false);
-        }
-            return view;
-        }
+
+    private Boolean isTokenValid(){
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("SPOTIFYAUTH", Context.MODE_PRIVATE);
+        return sharedPref.getBoolean("valid", false);
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+    ViewGroup view;
+    switch (pageNumber){
+        case 0:
+             view = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_onboarding_welcome, container, false);
+            break;
+        case 1:
+            view = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_onboarding_setup, container, false);
+            MaterialButton setup = view.findViewById(R.id.button_setup_spotify);
+            AtomicReference<AppCompatTextView> statusMessage =
+                    new AtomicReference<>(view.findViewById(R.id.onboarding_setup_status_message));
+            setup.setOnClickListener(v->{
+                statusMessage.set(view.findViewById(R.id.onboarding_setup_status_message));
+                if(isTokenValid()){
+                    statusMessage.get().setText(getText(R.string.onboarding_setup_status_success).toString());
+                    setup.setVisibility(View.INVISIBLE);
+                } else {
+                    statusMessage.get().setText(getText(R.string.onboarding_setup_status_error).toString());
+                    statusMessage.get().setTextColor(getResources().getColor(R.color.conjure_purple_really_dark));
+                }
+            });
+
+            break;
+        case 2:
+             view = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_onboarding_scan, container, false);
+            MaterialButton finish = view.findViewById(R.id.button_finish);
+            finish.setOnClickListener(v -> {
+                System.out.println("recording user finished onboarding");
+                SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getSharedPreferences("NEWUSER",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("newUser", false);
+                editor.apply();
+                System.out.println("starting HomeActivity");
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
+            });
+            break;
+        default:
+            view = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_onboarding_welcome, container, false);
+    }
+        return view;
+    }
+
+}
--- app/src/main/java/com/berryspace/conjure/SplashScreen.java
+++ app/src/main/java/com/berryspace/conjure/SplashScreen.java
@@ -20,11 +20,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class SplashScreen extends AppCompatActivity {
-    private static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
-    private static final String CLIENT_ID = BuildConfig.SPOTIFY_CLIENT_ID;
-    private String mAccessToken;
-    private Call mCall;
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+
     private final static String TAG = "SplashScreen";
 
     @Override
@@ -39,11 +35,6 @@ public class SplashScreen extends AppCompatActivity {
         } else {
             Log.d(TAG, "returning user detected, proceeding to home");
         }
-
-//        Boolean validSpotifyId = checkSpotifyId();
-//        if(!validSpotifyId) {
-//            getSpotifyId();
-//        }
     }
 
     @Override
@@ -62,7 +53,6 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        cancelCall();
         super.onDestroy();
     }
 
@@ -71,74 +61,5 @@ public class SplashScreen extends AppCompatActivity {
         return sharedPref.getBoolean("newUser", true);
     }
 
-    public void getUserId(){
-        final String[] userId = new String[1];
-        userId[0] = "";
 
-        final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me")
-                .addHeader("Authorization", "Bearer " + mAccessToken)
-                .build();
-
-        cancelCall();
-        mCall = mOkHttpClient.newCall(request);
-
-        mCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d(TAG, "Failed to fetch data: " + e);
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Intent intent = new Intent(getBaseContext(), HomeActivity.class);
-                try {
-                    assert response.body() != null;
-                    final JSONObject jsonObject = new JSONObject(response.body().string());
-                    userId[0] = jsonObject.get("id").toString();
-
-                    intent.putExtra("userId", userId[0]);
-                    intent.putExtra("token", mAccessToken);
-                    startActivity(intent);
-                } catch (JSONException error) {
-                    Log.d(TAG, error.toString());
-                    startActivity(intent);
-                }
-            }
-        });
-    };
-
-    public void getSpotifyId(){
-        final AuthorizationRequest request = getAuthenticationRequest(AuthorizationResponse.Type.TOKEN);
-        AuthorizationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request);
-    };
-
-    private AuthorizationRequest getAuthenticationRequest(AuthorizationResponse.Type type) {
-        return new AuthorizationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
-                .setShowDialog(true)
-                .setScopes(new String[]{"user-follow-read"})
-                .build();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        final AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, data);
-        if (requestCode == AUTH_TOKEN_REQUEST_CODE) {
-            mAccessToken = response.getAccessToken();
-            getUserId();
-        }
-    }
-
-    private void cancelCall() {
-        if (mCall != null) {
-            mCall.cancel();
-        }
-    }
-
-    private Uri getRedirectUri() {
-        return new Uri.Builder()
-                .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
-                .authority(getString(R.string.com_spotify_sdk_redirect_host))
-                .build();
-    }
 }
