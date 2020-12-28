cb91a5f26ab1e1261db7dcc5299060ba1d0e2f1f
James-W-Berry <james.berry.iv@gmail.com>
2020-12-28 18:51:57 -0500

    adjust base color styling

--- app/src/main/java/com/berryspace/conjure/HomeActivity.java
+++ app/src/main/java/com/berryspace/conjure/HomeActivity.java
@@ -10,33 +10,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdLoader;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.VideoController;
-import com.google.android.gms.ads.VideoOptions;
-import com.google.android.gms.ads.formats.MediaView;
-import com.google.android.gms.ads.formats.NativeAdOptions;
-import com.google.android.gms.ads.formats.UnifiedNativeAd;
-import com.google.android.gms.ads.formats.UnifiedNativeAdView;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -94,29 +73,22 @@ public class HomeActivity extends AppCompatActivity {
     Handler handler = new Handler();
     private Integer artistProcessDelay = 10000;
 
-    private static final String ADMOB_AD_UNIT_ID = "ca-app-pub-3940256099942544/2247696110";
-
-    private Button refresh;
-    private CheckBox startVideoAdsMuted;
-    private TextView videoStatus;
-    private UnifiedNativeAd nativeAd;
-
     private TextView artistStat;
     private TextView albumsStat;
-
     private String[] artistLibrary;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //setupAds();
 
         mFunctions = FirebaseFunctions.getInstance();
 
+        // get local app storage for Spotify credentials
         mUserId = getUser();
         mAccessToken = getToken();
+
+        // populate user library stats
         artistStat = findViewById(R.id.artistStat);
         artistStat.setOnClickListener(v ->{
             Intent intent = new Intent(this, LibraryActivity.class);
@@ -194,218 +166,8 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (nativeAd != null) {
-            nativeAd.destroy();
-        }
         handler.removeCallbacksAndMessages(null);
     }
-
-//    private void setupAds(){
-//        // Initialize the Mobile Ads SDK.
-//        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-//            @Override
-//            public void onInitializationComplete(InitializationStatus initializationStatus) {}
-//        });
-//
-//        refresh = findViewById(R.id.btn_refresh);
-//        startVideoAdsMuted = findViewById(R.id.cb_start_muted);
-//        videoStatus = findViewById(R.id.tv_video_status);
-//
-//        refresh.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View unusedView) {
-//                refreshAd();
-//            }
-//        });
-//
-//        refreshAd();
-//    }
-//
-//    /**
-//     * Populates a {@link UnifiedNativeAdView} object with data from a given
-//     * {@link UnifiedNativeAd}.
-//     *
-//     * @param nativeAd the object containing the ad's assets
-//     * @param adView          the view to be populated
-//     */
-//    private void populateUnifiedNativeAdView(UnifiedNativeAd nativeAd, UnifiedNativeAdView adView) {
-//        // Set the media view.
-//        adView.setMediaView((MediaView) adView.findViewById(R.id.ad_media));
-//
-//        // Set other ad assets.
-//        adView.setHeadlineView(adView.findViewById(R.id.ad_headline));
-//        adView.setBodyView(adView.findViewById(R.id.ad_body));
-//        adView.setCallToActionView(adView.findViewById(R.id.ad_call_to_action));
-//        adView.setIconView(adView.findViewById(R.id.ad_app_icon));
-//        adView.setPriceView(adView.findViewById(R.id.ad_price));
-//        adView.setStarRatingView(adView.findViewById(R.id.ad_stars));
-//        adView.setStoreView(adView.findViewById(R.id.ad_store));
-//        adView.setAdvertiserView(adView.findViewById(R.id.ad_advertiser));
-//
-//        // The headline and mediaContent are guaranteed to be in every UnifiedNativeAd.
-//        ((TextView) adView.getHeadlineView()).setText(nativeAd.getHeadline());
-//        adView.getMediaView().setMediaContent(nativeAd.getMediaContent());
-//
-//        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
-//        // check before trying to display them.
-//        if (nativeAd.getBody() == null) {
-//            adView.getBodyView().setVisibility(View.INVISIBLE);
-//        } else {
-//            adView.getBodyView().setVisibility(View.VISIBLE);
-//            ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
-//        }
-//
-//        if (nativeAd.getCallToAction() == null) {
-//            adView.getCallToActionView().setVisibility(View.INVISIBLE);
-//        } else {
-//            adView.getCallToActionView().setVisibility(View.VISIBLE);
-//            ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
-//        }
-//
-//        if (nativeAd.getIcon() == null) {
-//            adView.getIconView().setVisibility(View.GONE);
-//        } else {
-//            ((ImageView) adView.getIconView()).setImageDrawable(
-//                    nativeAd.getIcon().getDrawable());
-//            adView.getIconView().setVisibility(View.VISIBLE);
-//        }
-//
-//        if (nativeAd.getPrice() == null) {
-//            adView.getPriceView().setVisibility(View.INVISIBLE);
-//        } else {
-//            adView.getPriceView().setVisibility(View.VISIBLE);
-//            ((TextView) adView.getPriceView()).setText(nativeAd.getPrice());
-//        }
-//
-//        if (nativeAd.getStore() == null) {
-//            adView.getStoreView().setVisibility(View.INVISIBLE);
-//        } else {
-//            adView.getStoreView().setVisibility(View.VISIBLE);
-//            ((TextView) adView.getStoreView()).setText(nativeAd.getStore());
-//        }
-//
-//        if (nativeAd.getStarRating() == null) {
-//            adView.getStarRatingView().setVisibility(View.INVISIBLE);
-//        } else {
-//            ((RatingBar) adView.getStarRatingView())
-//                    .setRating(nativeAd.getStarRating().floatValue());
-//            adView.getStarRatingView().setVisibility(View.VISIBLE);
-//        }
-//
-//        if (nativeAd.getAdvertiser() == null) {
-//            adView.getAdvertiserView().setVisibility(View.INVISIBLE);
-//        } else {
-//            ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
-//            adView.getAdvertiserView().setVisibility(View.VISIBLE);
-//        }
-//
-//        // This method tells the Google Mobile Ads SDK that you have finished populating your
-//        // native ad view with this native ad.
-//        adView.setNativeAd(nativeAd);
-//
-//        // Get the video controller for the ad. One will always be provided, even if the ad doesn't
-//        // have a video asset.
-//        VideoController vc = nativeAd.getVideoController();
-//
-//        // Updates the UI to say whether or not this ad has a video asset.
-//        if (vc.hasVideoContent()) {
-//            videoStatus.setText(String.format(Locale.getDefault(),
-//                    "Video status: Ad contains a %.2f:1 video asset.",
-//                    vc.getAspectRatio()));
-//
-//            // Create a new VideoLifecycleCallbacks object and pass it to the VideoController. The
-//            // VideoController will call methods on this object when events occur in the video
-//            // lifecycle.
-//            vc.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
-//                @Override
-//                public void onVideoEnd() {
-//                    // Publishers should allow native ads to complete video playback before
-//                    // refreshing or replacing them with another ad in the same UI location.
-//                    refresh.setEnabled(true);
-//                    videoStatus.setText("Video status: Video playback has ended.");
-//                    super.onVideoEnd();
-//                }
-//            });
-//        } else {
-//            videoStatus.setText("Video status: Ad does not contain a video asset.");
-//            refresh.setEnabled(true);
-//        }
-//    }
-//
-//    /**
-//     * Creates a request for a new native ad based on the boolean parameters and calls the
-//     * corresponding "populate" method when one is successfully returned.
-//     *
-//     */
-//    private void refreshAd() {
-//        refresh.setEnabled(false);
-//
-//        AdLoader.Builder builder = new AdLoader.Builder(this, ADMOB_AD_UNIT_ID);
-//
-//        builder.forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
-//            // OnUnifiedNativeAdLoadedListener implementation.
-//            @Override
-//            public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-//                // If this callback occurs after the activity is destroyed, you must call
-//                // destroy and return or you may get a memory leak.
-//                if (isDestroyed()) {
-//                    unifiedNativeAd.destroy();
-//                    return;
-//                }
-//                // You must call destroy on old ads when you are done with them,
-//                // otherwise you will have a memory leak.
-//                if (nativeAd != null) {
-//                    nativeAd.destroy();
-//                }
-//                nativeAd = unifiedNativeAd;
-//                FrameLayout frameLayout =
-//                        findViewById(R.id.fl_adplaceholder);
-//                UnifiedNativeAdView adView = (UnifiedNativeAdView) getLayoutInflater()
-//                        .inflate(R.layout.advertisement, null);
-//                populateUnifiedNativeAdView(unifiedNativeAd, adView);
-//                frameLayout.removeAllViews();
-//                frameLayout.addView(adView);
-//            }
-//
-//        });
-//
-//        VideoOptions videoOptions = new VideoOptions.Builder()
-//                .setStartMuted(startVideoAdsMuted.isChecked())
-//                .build();
-//
-//        NativeAdOptions adOptions = new NativeAdOptions.Builder()
-//                .setVideoOptions(videoOptions)
-//                .build();
-//
-//        builder.withNativeAdOptions(adOptions);
-//
-//        AdLoader adLoader =
-//            builder
-//                .withAdListener(
-//                    new AdListener() {
-//                        @Override
-//                        public void onAdFailedToLoad(LoadAdError loadAdError) {
-//                            refresh.setEnabled(true);
-//                            String error =
-//                                    String.format(
-//                                            "domain: %s, code: %d, message: %s",
-//                                            loadAdError.getDomain(),
-//                                            loadAdError.getCode(),
-//                                            loadAdError.getMessage());
-//                            Toast.makeText(
-//                                    HomeActivity.this,
-//                                    "Failed to load native ad with error " + error,
-//                                    Toast.LENGTH_SHORT)
-//                                    .show();
-//                        }
-//                    })
-//                .build();
-//
-//        adLoader.loadAd(new AdRequest.Builder().build());
-//
-//        videoStatus.setText("");
-//    }
-
 
     private String getUser(){
         SharedPreferences sharedPref = this.getSharedPreferences("SPOTIFYAUTH", Context.MODE_PRIVATE);
